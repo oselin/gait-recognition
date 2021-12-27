6ceb3cb0665936aef9acced5ff4038b6b46eae67
MatteoAlbi <matteo.albi99.com>
2021-12-27 21:28:06 +0100

    added default transmit data in LpmsBT-setTransmitData added accuracy estimation in simulateStream QuatW value no more used for the net training

--- include/simulateStream.m
+++ include/simulateStream.m
@@ -7,7 +7,9 @@ function [] = simulateStream(network ,testData, reset_label)
     % ---------------------------------------------------------------------
 
     TIMEFRAME = 350; %[ms]
-    DATASTREAM = zeros(13, TIMEFRAME);%specifically for this kind of IMU SENSOR
+    DATASTREAM = zeros(12, TIMEFRAME);%specifically for this kind of IMU SENSOR
+    results = zeros(height(testData),1); 
+    testData = detectPhases_new_2(testData);%labeling
 
     y_to_plot = zeros(1,TIMEFRAME);
     
@@ -15,13 +17,14 @@ function [] = simulateStream(network ,testData, reset_label)
 
         %New acquisition
         new_stream = testData{i,:};
-        %Get rid of useless values
-        useless_data = [1:3 10:12 17:19 23:26];
+        %Get rid of useless values, label too
+        useless_data = [1:3 10:12 16:19 23:27];
         new_stream(:,useless_data) = [];
         %Update the stream
         DATASTREAM = [DATASTREAM(:,2:end) new_stream'];
         %Predict the label
         y = classify(network, DATASTREAM);
+          
 
         to_plot = DATASTREAM(3,:);
         time = i:i+TIMEFRAME-1;
@@ -29,10 +32,15 @@ function [] = simulateStream(network ,testData, reset_label)
             y_to_plot = grp2idx(y)';
         else
             y_to_plot = [y_to_plot(2:end) grp2idx(y(end))];
+            results(i) = grp2idx(y(end));
         end
 
-        plotLabeledData([to_plot' y_to_plot'], time);
-        xlim([i i+TIMEFRAME-1])
-        pause(0.01);
+%         plotLabeledData([to_plot' y_to_plot'], time);
+%         xlim([i i+TIMEFRAME-1]);
+%         pause(0.01);
     end
+    acc = sum(results == table2array(testData(:,end)))/height(testData);
+    disp(size(table2array(testData(:,end))));
+    disp(size(results));
+    disp(acc);
 end
